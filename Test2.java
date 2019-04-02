55e8671b109bb6362f69403b3f264bb19683aa61
lz1998 <875543533@qq.com>
2019-04-02 14:36:26 +0100

    problem2 not completed

---
+++ Test2.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet("/register.action")
+public class Test extends HttpServlet{
+   /* public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{*/
+		//PrintWriter out = response.getWriter();
+        
+        
+		//out.println("<html>");
+		//out.println("<body>");
+		//out.println("<h1>H123llo wd1</h1>");
+		//out.println("</body>");
+		//out.println("</html>");
+	//}
+
+    public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+        String userName=request.getParameter("username");
+    
+        //response.setContent("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+		out.println("<html>");
+        out.println("<head>");
+        out.println("<title>User Info</title>");
+        out.println("</head>");
+		out.println("<body>");
+		out.println("<h3>"+userName+"</h3>");
+		out.println("</body>");
+		out.println("</html>");
+        //out.println();
+    }
+
+}
